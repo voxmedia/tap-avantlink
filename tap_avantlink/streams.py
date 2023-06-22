856c83398fa4d3f8e66af85f68f78d97e32d51f6
Hunter Kuffel <hkuffel1217@gmail.com>
2023-06-22 16:17:17 -0700

    comma

--- tap_avantlink/streams.py
+++ tap_avantlink/streams.py
@@ -142,7 +142,7 @@ class SalesStream(TapAvantlinkStream):
         for float_col in [
             'Total_Commission', 'Incentive_Commission', 'Base_Commission', 'Transaction_Amount'
         ]:
-            sanitized_row[float_col] = sanitized_row[float_col].replace('$', '').replace('(', '-').replace(')', '')
+            sanitized_row[float_col] = sanitized_row[float_col].replace('$', '').replace('(', '-').replace(')', '').replace(',', '')
             sanitized_row[float_col] = set_none_or_cast(sanitized_row[float_col], float)
         return sanitized_row
 
@@ -219,6 +219,6 @@ class SalesHitsStream(TapAvantlinkStream):
         sanitized_row = super().post_process(row, context)
         sanitized_row['Date'] = datetime.strftime(datetime.strptime(sanitized_row['Date'], '%m/%d/%Y %H:%M'), '%Y-%m-%d %H:%M:%S')
         if 'Transaction_Amount' in sanitized_row:
-            sanitized_row['Transaction_Amount'] = sanitized_row['Transaction_Amount'].replace('$', '').replace('(', '-').replace(')', '')
+            sanitized_row['Transaction_Amount'] = sanitized_row['Transaction_Amount'].replace('$', '').replace('(', '-').replace(')', '').replace(',', '')
             sanitized_row['Transaction_Amount'] = float(sanitized_row['Transaction_Amount'])
         return sanitized_row
